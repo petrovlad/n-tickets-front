d5c795fde19f4a4474411073c4fea556ff2967f4
vladi <vladislav_petrovec@mail.ru>
2021-08-15 15:16:39 +0300

    fixed typo in redirect url

--- src/pages/ShowTickets.js
+++ src/pages/ShowTickets.js
@@ -1,5 +1,7 @@
-import React, {Fragment} from "react";
+import React from "react";
 import TicketsList from "../components/TicketsList";
+import AuthContext from "../context/auth-context";
+import {Redirect} from "react-router-dom";
 
 export const ShowTickets = () => {
   const tickets = new Array(3)
@@ -7,8 +9,14 @@ export const ShowTickets = () => {
     .map((_, index) => ({id: index, title:`Ticket ${index}`}));
 
   return (
-    <Fragment>
-      <TicketsList tickets={tickets}/>
-    </Fragment>
+    <AuthContext.Consumer>
+      {({signedIn, setSignedIn}) => (
+        <div className="p-5 mb-4 bg-light rounded-3">
+          {signedIn ? <TicketsList tickets={tickets}/> : <Redirect to="/signin"/>}
+        </div>
+
+      )}
+    </AuthContext.Consumer>
+
   )
 }
