f7ad3401e99d07082d6445dcd88b705cd2a5f36f
vladi <vladislav_petrovec@mail.ru>
2021-08-15 20:51:20 +0300

    some changes in ticket component

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -3,6 +3,6 @@ import React from "react";
 export const Ticket = (props) => {
   const ticket = props.value;
   return (ticket ?
-      <p>{ticket.title}</p> : <p>You have no tickets yet</p>
+      <p>{ticket.title}-{ticket.uniqueHash}</p> : <p>Ticket doesn't exist</p>
   )
 }
