b5a9ba1369061af9302903e8b426d314c9a088b5
vladi <vladislav_petrovec@mail.ru>
2021-08-16 02:48:37 +0300

    implemented put ticket function

--- src/services/api/tickets-service.js
+++ src/services/api/tickets-service.js
@@ -5,11 +5,19 @@ import config from '../../config/config.json'
 export const getTickets = () => {
   const apiUrl = config.server + '/tickets';
   return axios.get(apiUrl, getConfig())
-    .then(response => {
-      return response.data
-    })
+    .then(response => response.data)
     .catch(err => {
       console.log(err)
     });
 }
 
+export const putTicket = (ticket) => {
+  const apiUrl = config.server + "/tickets/" + ticket.uniqueHash;
+  console.log(ticket);
+  return axios.put(apiUrl, ticket, getConfig())
+    .then(response => {
+      console.log(response)
+      return response
+    })
+    .catch(err => {console.log(err)})
+}
