996703b7d73398ad0bfcee8309d4f5685090e559
vladi <vladislav_petrovec@mail.ru>
2021-08-16 05:20:32 +0300

    playing with styles

--- src/components/modals/DeleteModalForm.js
+++ src/components/modals/DeleteModalForm.js
@@ -6,18 +6,25 @@ export const DeleteModalForm = () => {
   return (
     <div className="modal fade" id="exampleDeleteModal" tabIndex="-1" aria-labelledby="exampleModalCenterTitle"
          style={{display: "none"}} aria-modal="true" role="dialog">
-      <div className="modal-dialog">
+      <div className="modal-dialog modal-dialog-centered">
         <div className="modal-content rounded-4 shadow">
+          <div className="modal-header text-center">
+              <h5 className="modal-title w-100 mb-0">Are you sure you want to delete?</h5>
+          </div>
           <div className="modal-body p-4 text-center">
-            <h5 className="mb-0">Enable this setting?</h5>
-            <p className="mb-0">You can always change your mind in your account settings.</p>
+            <p className="mb-0">This action cannot be undone.</p>
           </div>
           <div className="modal-footer flex-nowrap p-0">
             <button type="button"
-                    className="btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0 border-right"><strong>Yes,
-              enable</strong></button>
-            <button type="button" className="btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0"
-                    data-bs-dismiss="modal">No thanks
+                    className="btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0 border-right">
+              <strong>
+                Yes, delete
+              </strong>
+            </button>
+            <button type="button"
+                    className="btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0"
+                    data-bs-dismiss="modal">
+              Cancel
             </button>
           </div>
         </div>
