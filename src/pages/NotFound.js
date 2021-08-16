ff88a48d24586601e2bcdc749f104216bb6c7795
vladi <vladislav_petrovec@mail.ru>
2021-08-17 02:45:54 +0300

    added message

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,10 @@
 import React, {Fragment} from "react";
 import {NavLink} from "react-router-dom";
 
-export const NotFound = () => {
+export const NotFound = (props) => {
   return (
     <Fragment>
+      <h3>{props.message}</h3>
       <NavLink to="/">Go home.</NavLink>
     </Fragment>
   )
