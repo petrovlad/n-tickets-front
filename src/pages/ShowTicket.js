96ef0f36f9b43f50881da9b0c5713d208a4bd54d
vladi <vladislav_petrovec@mail.ru>
2021-08-17 02:49:00 +0300

    changed show ticket ui

--- src/pages/ShowTicket.js
+++ src/pages/ShowTicket.js
@@ -2,21 +2,20 @@ import React, {useEffect, useState} from "react";
 import AuthContext from "../context/auth-context";
 import {Redirect, useParams} from "react-router-dom";
 import {getTicket} from "../services/api/show-ticket-service";
-import {Ticket} from "../components/Ticket";
+import {ErrorPage} from "./ErrorPage";
 
 export const ShowTicket = () => {
   let { hash } = useParams();
 
   const [ticket, setTicket] = useState([]);
   const [flag, setFlag] = useState(false);
+  const [err, setErr] = useState(null);
 
   useEffect(() => {
     if (!flag) {
       setFlag(true);
-      getTicket(hash).then(result => {
-        console.log(result);
-        setTicket(result);
-      })
+      getTicket(hash).then(setTicket)
+        .catch(setErr);
     }
   }, [flag, ticket]);
 
@@ -24,7 +23,57 @@ export const ShowTicket = () => {
     <AuthContext.Consumer>
       {({signedIn, setSignedIn}) => (
         <div className="p-5 mb-4 bg-light rounded-3">
-          {signedIn ? <Ticket value={ticket}/> : <Redirect to="/signin"/>}
+          {!signedIn ? <Redirect to="/signin"/>
+            :
+            <>
+              {err !== null ? <ErrorPage message={err.message}/>
+                :
+                <>
+                    <div className="mb-3 row">
+                      <div className="input-group">
+                        <span className="input-group-text" id="basic-addon1">Title</span>
+                        <input type="text"
+                               id="title"
+                               name="title"
+                               className="form-control"
+                               readOnly
+                               aria-describedby="basic-addon1"
+                               defaultValue={ticket.title}
+                               required/>
+                      </div>
+                    </div>
+
+                    <div className="mb-3 row">
+                      <div className="input-group">
+                        <span className="input-group-text" id="basic-addon1">Content</span>
+                        <textarea className="form-control"
+                                  readOnly
+                                  aria-label="With textarea"
+                                  id="content"
+                                  aria-describedby="basic-addon1"
+                                  defaultValue={ticket.content}
+                                  required/>
+                      </div>
+                    </div>
+
+                    {ticket.showWarning &&
+                    <div className="mb-3 row">
+                      <div className="input-group">
+                        <span className="input-group-text" id="basic-addon-count">Readings count</span>
+                        <input type="number"
+                               id="count"
+                               readOnly
+                               name="count"
+                               className="form-control"
+                               defaultValue={ticket.readingsCount}
+                               required/>
+                      </div>
+                    </div>
+                    }
+                </>
+              }
+            </>
+          }
         </div>
       )}
     </AuthContext.Consumer>
