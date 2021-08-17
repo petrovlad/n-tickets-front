6ddd86518e45302bf8079b1bd25a7141e6a78f04
vladi <vladislav_petrovec@mail.ru>
2021-08-17 07:22:24 +0300

    make text centered again

--- src/components/TicketsList.js
+++ src/components/TicketsList.js
@@ -1,16 +1,19 @@
 import React from "react";
 import {Ticket} from "./Ticket";
+import isEmpty from "./util/object-utils";
 
 export const TicketsList = (props) => {
   const tickets = props.tickets;
 
-  return (tickets ?
-    <ul className="list-group" style={{listStyleType: "none"}}>
-      {tickets.sort((a, b) => a.uniqueHash.localeCompare(b.uniqueHash)).map((ticket) => (
-        <li key={ticket.uniqueHash}  style={{marginBottom: "15px"}}>
-          <Ticket value={ticket}/>
-        </li>
-      ))}
-    </ul> : <p>You have no tickets yet</p>
+  return (!isEmpty(tickets) ?
+      <ul className="list-group" style={{listStyleType: "none"}}>
+        {tickets.sort((a, b) => a.uniqueHash.localeCompare(b.uniqueHash)).map((ticket) => (
+          <li key={ticket.uniqueHash}  style={{marginBottom: "15px"}}>
+            <Ticket value={ticket}/>
+          </li>
+        ))}
+      </ul>
+      :
+      <p className="text-center">You have no tickets yet.</p>
   )
 }
