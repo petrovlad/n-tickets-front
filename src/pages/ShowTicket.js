af36130d7d8ccef44c9a3fe42f0d015dd811a6c2
vladi <vladislav_petrovec@mail.ru>
2021-08-17 09:24:54 +0300

    removed unauthenticated redirects

--- src/pages/ShowTicket.js
+++ src/pages/ShowTicket.js
@@ -22,69 +22,60 @@ export const ShowTicket = () => {
   }, [flag, ticket]);
 
   return (
-    <AuthContext.Consumer>
-      {({signedIn, setSignedIn}) => (
         <div className="p-5 mb-4 bg-light rounded-3">
-          {!signedIn ? <Redirect to="/signin"/>
+          {err === null && isEmpty(ticket) ?
+            <LinearProgress/>
             :
             <>
-              {err === null && isEmpty(ticket) ?
-                <LinearProgress/>
+              {err !== null ?
+                <ErrorPage message={err.message}/>
                 :
                 <>
-                  {err !== null ?
-                    <ErrorPage message={err.message}/>
-                    :
-                    <>
-                      <div className="mb-3 row">
-                        <div className="input-group">
-                          <span className="input-group-text" id="basic-addon1">Title</span>
-                          <input type="text"
-                                 id="title"
-                                 name="title"
-                                 className="form-control"
-                                 readOnly
-                                 aria-describedby="basic-addon1"
-                                 defaultValue={ticket.title}
-                                 required/>
-                        </div>
-                      </div>
+                  <div className="mb-3 row">
+                    <div className="input-group">
+                      <span className="input-group-text" id="basic-addon1">Title</span>
+                      <input type="text"
+                             id="title"
+                             name="title"
+                             className="form-control"
+                             readOnly
+                             aria-describedby="basic-addon1"
+                             defaultValue={ticket.title}
+                             required/>
+                    </div>
+                  </div>
 
-                      <div className="mb-3 row">
-                        <div className="input-group">
-                          <span className="input-group-text" id="basic-addon1">Content</span>
-                          <textarea className="form-control"
-                                    readOnly
-                                    aria-label="With textarea"
-                                    id="content"
-                                    aria-describedby="basic-addon1"
-                                    defaultValue={ticket.content}
-                                    required/>
-                        </div>
-                      </div>
+                  <div className="mb-3 row">
+                    <div className="input-group">
+                      <span className="input-group-text" id="basic-addon1">Content</span>
+                      <textarea className="form-control"
+                                readOnly
+                                aria-label="With textarea"
+                                id="content"
+                                aria-describedby="basic-addon1"
+                                defaultValue={ticket.content}
+                                required/>
+                    </div>
+                  </div>
 
-                      {ticket.showWarning &&
-                      <div className="mb-3 row">
-                        <div className="input-group">
-                          <span className="input-group-text" id="basic-addon-count">Readings count</span>
-                          <input type="number"
-                                 id="count"
-                                 readOnly
-                                 name="count"
-                                 className="form-control"
-                                 defaultValue={ticket.readingsCount}
-                                 required/>
-                        </div>
-                      </div>
-                      }
-                    </>
+                  {ticket.showWarning &&
+                  <div className="mb-3 row">
+                    <div className="input-group">
+                      <span className="input-group-text" id="basic-addon-count">Readings count</span>
+                      <input type="number"
+                             id="count"
+                             readOnly
+                             name="count"
+                             className="form-control"
+                             defaultValue={ticket.readingsCount}
+                             required/>
+                    </div>
+                  </div>
                   }
                 </>
               }
             </>
           }
         </div>
-      )}
-    </AuthContext.Consumer>
   )
 }
