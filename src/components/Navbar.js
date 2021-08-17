1d27908ebd0140299e6cd0ef12e7dae90af93440
vladi <vladislav_petrovec@mail.ru>
2021-08-17 07:51:43 +0300

    fixed typo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export const Navbar = () => {
       {({signedIn, setSignedIn}) => (
       <nav className="navbar navbar-dark navbar-expand-lg bg-secondary">
         <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">NTickets</NavLink>
+          <NavLink className="navbar-brand" to="/">N-Tickets</NavLink>
 
           <button className="navbar-toggler me-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                   aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
